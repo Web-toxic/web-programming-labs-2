5bafc3f64e6f63c496a5c0aecba8037ccf330306
Полина Топорнина <ptopornina@gmail.com>
2023-09-19 13:19:07 +0700

    Наследование шаблонов

--- app.py
+++ app.py
@@ -241,3 +241,7 @@ def example():
     return render_template('example.html',
                            name=name, lab=num_of_lab, group=group, 
                            course=num_of_course, fruits=fruits, books=books)
+
+@app.route('/lab2/')
+def lab2():
+    return render_template('lab2.html')
