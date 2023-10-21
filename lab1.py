ebcfd1b926c4f0d92c086dfdf35950f289088a4d
Полина Топорнина <ptopornina@gmail.com>
2023-10-21 15:53:20 +0700

    Защита 2 и 3 лр

--- lab1.py
+++ lab1.py
@@ -34,6 +34,12 @@ def menu():
                 <li>
                     <a href="http://127.0.0.1:5000/lab3" target="_blank">Лабораторная работа 3</a>
                 </li>
+                <li>
+                    <a href="http://127.0.0.1:5000/lab4" target="_blank">Лабораторная работа 4</a>
+                </li>
+                <li>
+                    <a href="http://127.0.0.1:5000/zzz" target="_blank">Защита</a>
+                </li>
             </ul>
         </main>
 
