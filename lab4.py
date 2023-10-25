98b7880e6770ee8454e4761969bbdcb1077f6f62
Полина Топорнина <ptopornina@gmail.com>
2023-10-25 15:17:04 +0700

    Авторизация

--- lab4.py
+++ lab4.py
@@ -5,3 +5,24 @@ lab4 = Blueprint('lab4', __name__)
 @lab4.route('/lab4/')
 def lab():
     return render_template('lab4.html')
+
+
+@lab4.route('/lab4/login', methods = ['GET', 'POST'])
+def login():
+    if request.method == 'GET':
+        return render_template("login.html")
+    
+
+    username = request.form.get('username')
+    password = request.form.get('password')
+    if username == 'alex' and password == '123':
+        return render_template('success.html')
+    errors = 'Неверные логин или пароль'
+    if username == '':
+        errors = 'Введите логин'
+    if password == '':
+        errors = 'Введите пароль'
+    
+    
+    return render_template('login.html', errors=errors, username=username, password=password)
+ 
