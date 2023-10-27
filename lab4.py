86f1a2f5f6d6f8828a0a06ab0aa30f8ebd13c6d2
Полина Топорнина <ptopornina@gmail.com>
2023-10-27 12:49:52 +0700

    Заказ зерна

--- lab4.py
+++ lab4.py
@@ -50,3 +50,47 @@ def refrigerator():
         elif (temperature >= (-4)) and (temperature <= (-1)):
             err = f'Установлена температура: {temperature}°С ❄️'
     return render_template('refrigerator.html', temperature=temperature, err=err)
+
+
+@lab4.route('/lab4/grain_order')
+def grain_order():
+    return render_template('grain_order.html')
+
+
+@lab4.route('/process_order', methods=['POST'])
+def process_order():
+    grain_type = request.form['grainType']
+    weight = request.form['weight']
+
+    if not weight:
+        return "Ошибка: не введён вес. <a href='/lab4/grain_order'>Вернуться к заказу</a>"
+    
+    weight = float(weight)
+
+    number = 0
+
+    if weight <= 0:
+        return "Ошибка: неверное значение веса. <a href='/lab4/grain_order'>Вернуться к заказу</a>"
+
+    price_per_ton = 0
+    if grain_type == 'ячмень':
+        price_per_ton = 12000
+    elif grain_type == 'овёс':
+        price_per_ton = 8500
+    elif grain_type == 'пшеница':
+        price_per_ton = 8700
+    elif grain_type == 'рожь':
+        price_per_ton = 14000
+
+    if weight > 500:
+        return "Ошибка: такого объёма сейчас нет в наличии. <a href='/lab4/grain_order'>Вернуться к заказу</a>"
+    elif weight > 50:
+        total_price = weight * price_per_ton * 0.9
+        message = f"Заказ успешно сформирован. Вы заказали зерно: {grain_type}. Вес: {weight} т. Сумма к оплате: {total_price} руб. (Применена скидка 10% за большой объём)"
+    else:
+        total_price = weight * price_per_ton
+        message = f"Заказ успешно сформирован. Вы заказали зерно: {grain_type}. Вес: {weight} т. Сумма к оплате: {total_price} руб."
+
+    return f"<h3>{message}</h3><br><a href='/lab4/grain_order'>Вернуться к заказу</a>"
+
+
