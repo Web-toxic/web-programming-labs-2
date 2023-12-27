0430e6c200979c59e15054fe2146c2326e0080d9
Полина Топорнина <ptopornina@gmail.com>
2023-12-28 01:15:38 +0700

    Открытка

--- lab9.py
+++ lab9.py
@@ -2,9 +2,15 @@ from flask import Blueprint, render_template, request
 
 lab9=Blueprint('lab9',__name__)
 
-@lab9.route('/lab9/')
+@lab9.route('/lab9/', methods=['GET', 'POST'])
 def main():
-    return render_template('lab9/index.html')
+    if request.method =='GET':
+        return render_template('lab9/index.html')
+    if request.method == 'POST':
+        user_1 = request.form.get('user_1')
+        gender = request.form.get('gender')
+        user_2 = request.form.get('user_2')
+        return render_template('lab9/postcard.html', gender=gender, user_1=user_1, user_2=user_2)
 
 
 @lab9.app_errorhandler(404)
@@ -20,3 +26,4 @@ def server_error(err):
 @lab9.route('/lab9/500')
 def server_error():
     return render_template('lab9/500.html')
+
