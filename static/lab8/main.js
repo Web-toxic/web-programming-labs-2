8e20f8c76934c2535c8830a737bf12754328a345
Полина Топорнина <ptopornina@gmail.com>
2023-12-27 23:04:02 +0700

    Редактирование курсов

--- static/lab8/main.js
+++ static/lab8/main.js
@@ -97,3 +97,10 @@ function sendCourse(){
     });
 }
 
+function editCourse(num,course){
+    document.getElementById('num').value=num;
+    document.getElementById('name').value=course.name;
+    document.getElementById('videos').value=course.videos;
+    document.getElementById('price').value=course.price;
+    showModal();
+}
