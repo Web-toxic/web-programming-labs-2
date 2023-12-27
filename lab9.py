82a4c3dcb928e5e303c8cade66b44595510e205a
Полина Топорнина <ptopornina@gmail.com>
2023-12-28 00:38:46 +0700

    Обработчик ошибки 500

--- lab9.py
+++ lab9.py
@@ -6,6 +6,17 @@ lab9=Blueprint('lab9',__name__)
 def main():
     return render_template('lab9/index.html')
 
+
 @lab9.app_errorhandler(404)
 def not_found(err):
     return render_template('lab9/404.html'), 404
+
+
+@lab9.app_errorhandler(500)
+def server_error(err):
+    return render_template('lab9/500.html'), 500
+
+
+@lab9.route('/lab9/500')
+def server_error():
+    return render_template('lab9/500.html')
